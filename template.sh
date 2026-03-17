8a24812545e5b0a028d5bf44d2e0588fb2aea074
ABHIYANKKUMAR00 <abhiyankgujjar00@gmail.com>
2026-03-17 23:43:51 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+#Creating directory
+mkdir -p sr
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trias.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo"Directoru and files created successfully".
